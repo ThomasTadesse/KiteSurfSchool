9f00c12f043b116be31d900ae32a585ee604aa94
ThomasTadesse <328704@student.mboutrecht.nl>
2025-03-17 12:25:42 +0100

    images todo

--- KiteSurfSchool/resources/views/login.blade.php
+++ KiteSurfSchool/resources/views/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log in</title>
+    <title>Log in pagina</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body class="bg-gradient-to-br from-blue-50 to-blue-100 min-h-screen">
