c27b7dc1d726e92fdc1bfae74933b1df569db566
ThomasTadesse <328704@student.mboutrecht.nl>
2025-05-06 10:34:21 +0200

    login succes

--- KiteSurfSchool/routes/web.php
+++ KiteSurfSchool/routes/web.php
@@ -13,45 +13,13 @@ Route::get('/contact', function () {
 
 Route::get('/login', function () {
     return view('login');
-})->name('login')->middleware('guest');
+});
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
 
-// Klant (customer) protected routes
-Route::middleware(['auth', 'role.klant'])->group(function () {
-    Route::get('/profiel', function () {
-        return view('profiel');
-    });
-    
-    Route::get('/reserveringen', function () {
-        return view('reserveringen');
-    });
-});
-
-// Instructeur protected routes
-Route::middleware(['auth', 'role.instructeur'])->group(function () {
-    Route::get('/lesrooster', function () {
-        return view('lesrooster');
-    });
-    
-    Route::get('/mijn-studenten', function () {
-        return view('mijn-studenten');
-    });
-});
-
-// Eigenaar protected routes
-Route::middleware(['auth', 'role.eigenaar'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    });
-    
-    Route::get('/beheer/gebruikers', function () {
-        return view('beheer.gebruikers');
-    });
-    
-    Route::get('/beheer/inkomsten', function () {
-        return view('beheer.inkomsten');
-    });
-});
+// Add profile route
+Route::get('/profiel', function () {
+    return view('profiel');
+})->middleware('auth')->name('profile');
